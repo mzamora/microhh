89fea7a079a7b5fd14c8f91ef2f80ac7b1c9502e
Elynn Wu <elw014@panther31.dynamic.ucsd.edu>
2018-08-03 16:58:01 -0700

    adding pressure solver using multigrid

---
+++ src/pres_multigrid.cxx
@@ -0,0 +1,47 @@
+/*
+ * MicroHH
+ * Copyright (c) 2011-2017 Chiel van Heerwaarden
+ * Copyright (c) 2011-2017 Thijs Heus
+ * Copyright (c) 2014-2017 Bart van Stratum
+ * Copyright (c) Elynn Wu, Monica Zamora Zapata
+ *
+ * This file is part of MicroHH
+ *
+ * MicroHH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * MicroHH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with MicroHH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include ALL_HEADER_FILES
+
+Pres_multigrid::Pres_multigrid()
+{
+
+}
+
+//red-black smoothing  
+void Pres_multigrid::smooth(){
+
+}
+
+//interpolate to the next coarser grid, optional weighting (straight injection, half weighting, full weighting) 
+void Pres_multigrid::restrict(){
+
+}
+
+//interpolate to the next finer grid
+void Pres_multigrid::prolong(){
+	
+}
+
+//calculates the redisual
+void Pres_multigrid::residual(){
+	
+}
